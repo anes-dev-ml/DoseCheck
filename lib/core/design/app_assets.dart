74559be1c8834300a41e519893ef889a43c9994b
Anes Dev <sabrinaanes1234@gmail.com>
2026-08-10 02:29:57 -0400

    Centralize DoseCheck runtime asset paths

---
+++ lib/core/design/app_assets.dart
@@ -0,0 +1,10 @@
+abstract final class AppAssets {
+  static const brandMark = 'assets/branding/dosecheck_mark.png';
+
+  static const navToday = 'assets/icons/nav_today.png';
+  static const navHistory = 'assets/icons/nav_history.png';
+  static const navSettings = 'assets/icons/nav_settings.png';
+
+  static const dosePills = 'assets/icons/dose_pills.png';
+  static const doseInsulinNight = 'assets/icons/dose_insulin_night.png';
+}
