33f468d8958ca009f586341299acd7df8bdfce5f
Anes Dev <sabrinaanes1234@gmail.com>
2026-08-10 01:10:22 -0400

    Add responsive content frame

---
+++ lib/core/widgets/content_frame.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ContentFrame extends StatelessWidget {
+  const ContentFrame({
+    super.key,
+    required this.child,
+    this.maxWidth = 680,
+    this.padding = const EdgeInsets.fromLTRB(20, 20, 20, 32),
+  });
+
+  final Widget child;
+  final double maxWidth;
+  final EdgeInsetsGeometry padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: maxWidth),
+          child: Padding(
+            padding: padding,
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
