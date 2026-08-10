094c6c0c5febb200465600feee0cd3bd1718abe9
Anes Dev <sabrinaanes1234@gmail.com>
2026-08-10 01:18:59 -0400

    Inject application controller into features

--- lib/app/app_shell.dart
+++ lib/app/app_shell.dart
@@ -1,3 +1,4 @@
+import 'package:dosecheck/app/app_controller.dart';
 import 'package:dosecheck/features/history/presentation/history_page.dart';
 import 'package:dosecheck/features/settings/presentation/settings_page.dart';
 import 'package:dosecheck/features/today/presentation/today_page.dart';
@@ -5,7 +6,12 @@ import 'package:dosecheck/l10n/generated/app_localizations.dart';
 import 'package:flutter/material.dart';
 
 class AppShell extends StatefulWidget {
-  const AppShell({super.key});
+  const AppShell({
+    super.key,
+    required this.controller,
+  });
+
+  final DoseCheckController controller;
 
   @override
   State<AppShell> createState() => _AppShellState();
@@ -21,10 +27,10 @@ class _AppShellState extends State<AppShell> {
     return Scaffold(
       body: IndexedStack(
         index: _index,
-        children: const [
-          TodayPage(),
-          HistoryPage(),
-          SettingsPage(),
+        children: [
+          TodayPage(controller: widget.controller),
+          HistoryPage(controller: widget.controller),
+          SettingsPage(controller: widget.controller),
         ],
       ),
       bottomNavigationBar: NavigationBar(
