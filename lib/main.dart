0db58a2ce118178f8183699798a7af5779bc2e75
Anes Dev <sabrinaanes1234@gmail.com>
2026-08-10 01:19:20 -0400

    Bootstrap DoseCheck local state at startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:dosecheck/app/app.dart';
+import 'package:dosecheck/app/bootstrap_gate.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const DoseCheckApp());
+  runApp(const DoseCheckBootstrapGate());
 }
