a44b99c3b491196d5a4d68b637c5b7f0cc730d4c
Anes Dev <sabrinaanes1234@gmail.com>
2026-08-10 01:10:38 -0400

    Add three-destination application shell

---
+++ lib/app/app_shell.dart
@@ -0,0 +1,53 @@
+import 'package:dosecheck/features/history/presentation/history_page.dart';
+import 'package:dosecheck/features/settings/presentation/settings_page.dart';
+import 'package:dosecheck/features/today/presentation/today_page.dart';
+import 'package:dosecheck/l10n/generated/app_localizations.dart';
+import 'package:flutter/material.dart';
+
+class AppShell extends StatefulWidget {
+  const AppShell({super.key});
+
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  int _index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
+
+    return Scaffold(
+      body: IndexedStack(
+        index: _index,
+        children: const [
+          TodayPage(),
+          HistoryPage(),
+          SettingsPage(),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _index,
+        onDestinationSelected: (index) => setState(() => _index = index),
+        destinations: [
+          NavigationDestination(
+            icon: const Icon(Icons.today_outlined),
+            selectedIcon: const Icon(Icons.today_rounded),
+            label: l10n.navToday,
+          ),
+          NavigationDestination(
+            icon: const Icon(Icons.history_outlined),
+            selectedIcon: const Icon(Icons.history_rounded),
+            label: l10n.navHistory,
+          ),
+          NavigationDestination(
+            icon: const Icon(Icons.tune_outlined),
+            selectedIcon: const Icon(Icons.tune_rounded),
+            label: l10n.navSettings,
+          ),
+        ],
+      ),
+    );
+  }
+}
