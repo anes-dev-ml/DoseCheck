188d148f600d839a591697e0b98cef1fe9b04f4a
Anes Dev <sabrinaanes1234@gmail.com>
2026-08-10 02:50:11 -0400

    Strengthen in-app DoseCheck mark

--- lib/core/design/dosecheck_mark.dart
+++ lib/core/design/dosecheck_mark.dart
@@ -2,18 +2,24 @@ import 'package:dosecheck/core/design/app_assets.dart';
 import 'package:flutter/material.dart';
 
 class DoseCheckMark extends StatelessWidget {
-  const DoseCheckMark({super.key, this.size = 32});
+  const DoseCheckMark({super.key, this.size = 32, this.color});
 
   final double size;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      AppAssets.brandMark,
-      width: size,
-      height: size,
-      fit: BoxFit.contain,
-      excludeFromSemantics: true,
+    final resolvedColor = color ?? Theme.of(context).colorScheme.primary;
+
+    return ColorFiltered(
+      colorFilter: ColorFilter.mode(resolvedColor, BlendMode.srcIn),
+      child: Image.asset(
+        AppAssets.brandMark,
+        width: size,
+        height: size,
+        fit: BoxFit.contain,
+        excludeFromSemantics: true,
+      ),
     );
   }
 }
